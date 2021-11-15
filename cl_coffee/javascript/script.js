f4f4c19618f789686ea08b5a6452e7379c0627d0
Pudderton <90010848+Pudderton@users.noreply.github.com>
2021-11-14 19:53:39 -0500

    Added Javascript
    Added Javascript for email confirmation

--- cl_coffee/javascript/script.js
+++ cl_coffee/javascript/script.js
@@ -8,3 +8,26 @@ toggleButton.addEventListener('click', () => {
 
 // EMAIL
 
+function validateEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+
+function validate() {
+  const $result = $("#result");
+  const email = $("#email").val();
+  $result.text("");
+
+  if (validateEmail(email)) {
+    $result.text("An email has been sent for confimation! If you did not enter a name you will be asked to select one in the confirmation link.");
+    $result.css("color", "green");
+  } else {
+    $result.text("Email is not valid");
+    $result.css("color", "red");
+  }
+  return false;
+}
+
+document.getElementById("joinBtnClick").onclick = function() {validate()};
+
+//$("#email").on("input", validate);
